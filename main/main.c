48731748cab016699808e49d777773709a6a1ff4
hazem3443 <hazemkhaled3443@gmail.com>
2023-09-16 21:17:05 +0300

    update local files with latest data version

---
+++ main/main.c
@@ -0,0 +1,341 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "esp_timer.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "esp_http_client.h"
+
+#include "esp_netif.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+#define EXAMPLE_ESP_MAXIMUM_RETRY 10
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    // Set hostname for station interface
+    esp_netif_t *netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
+    esp_netif_set_hostname(netif, "Hazem_IAP");
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    // Set WiFi SSID and password
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "realme 6 Pro",
+            .password = "01015548853",
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 "realme 6 Pro", "01015548853");
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 "realme 6 Pro", "01015548853");
+    }
+    else
+    {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+}
+// #define WEB_SERVER "example.com"
+// #define WEB_PORT "80"
+// #define WEB_PATH "/"
+
+// static const char *REQUEST = "GET " WEB_PATH " HTTP/1.0\r\n"
+//                              "Host: " WEB_SERVER ":" WEB_PORT "\r\n"
+//                              "User-Agent: esp-idf/1.0 esp32\r\n"
+//                              "\r\n";
+
+// static void http_get_task(void *pvParameters)
+// {
+//     const struct addrinfo hints = {
+//         .ai_family = AF_INET,
+//         .ai_socktype = SOCK_STREAM,
+//     };
+//     struct addrinfo *res;
+//     struct in_addr *addr;
+//     int s, r;
+//     char recv_buf[64];
+
+//     while (1)
+//     {
+//         int err = getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res);
+
+//         if (err != 0 || res == NULL)
+//         {
+//             ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+//             vTaskDelay(1000 / portTICK_PERIOD_MS);
+//             continue;
+//         }
+
+//         /* Code to print the resolved IP.
+//            Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
+//         addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+//         ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+
+//         s = socket(res->ai_family, res->ai_socktype, 0);
+//         if (s < 0)
+//         {
+//             ESP_LOGE(TAG, "... Failed to allocate socket.");
+//             freeaddrinfo(res);
+//             vTaskDelay(1000 / portTICK_PERIOD_MS);
+//             continue;
+//         }
+//         ESP_LOGI(TAG, "... allocated socket");
+
+//         if (connect(s, res->ai_addr, res->ai_addrlen) != 0)
+//         {
+//             ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+//             close(s);
+//             freeaddrinfo(res);
+//             vTaskDelay(4000 / portTICK_PERIOD_MS);
+//             continue;
+//         }
+
+//         ESP_LOGI(TAG, "... connected");
+//         freeaddrinfo(res);
+
+//         if (write(s, REQUEST, strlen(REQUEST)) < 0)
+//         {
+//             ESP_LOGE(TAG, "... socket send failed");
+//             close(s);
+//             vTaskDelay(4000 / portTICK_PERIOD_MS);
+//             continue;
+//         }
+//         ESP_LOGI(TAG, "... socket send success");
+
+//         struct timeval receiving_timeout;
+//         receiving_timeout.tv_sec = 5;
+//         receiving_timeout.tv_usec = 0;
+//         if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
+//                        sizeof(receiving_timeout)) < 0)
+//         {
+//             ESP_LOGE(TAG, "... failed to set socket receiving timeout");
+//             close(s);
+//             vTaskDelay(4000 / portTICK_PERIOD_MS);
+//             continue;
+//         }
+//         ESP_LOGI(TAG, "... set socket receiving timeout success");
+
+//         /* Read HTTP response */
+//         do
+//         {
+//             bzero(recv_buf, sizeof(recv_buf));
+//             r = read(s, recv_buf, sizeof(recv_buf) - 1);
+//             for (int i = 0; i < r; i++)
+//             {
+//                 putchar(recv_buf[i]);
+//             }
+//         } while (r > 0);
+
+//         ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
+//         close(s);
+//         for (int countdown = 10; countdown >= 0; countdown--)
+//         {
+//             ESP_LOGI(TAG, "%d... ", countdown);
+//             vTaskDelay(5000 / portTICK_PERIOD_MS);
+//         }
+//         ESP_LOGI(TAG, "Starting again!");
+//     }
+// }
+
+// Main function
+// void log_task(void *pvParameters)
+// {
+//     while(1) {
+//         ESP_LOGI(TAG, "This log is added to test the periodicity of 100ms task. Timestamp: %lld", esp_timer_get_time());
+//         vTaskDelay(100 / portTICK_PERIOD_MS);
+//     }
+// }
+#include "esp_heap_caps.h"
+void download_and_log_file(const char *url)
+{
+    esp_http_client_config_t config = {
+        .url = url,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_open(client, 0);
+
+    ESP_LOGW(TAG, "download file %s free heapSize %d", url, heap_caps_get_largest_free_block(MALLOC_CAP_8BIT));
+
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to open HTTP connection: %s", esp_err_to_name(err));
+    }
+    else
+    {
+        int content_length = esp_http_client_fetch_headers(client);
+        ESP_LOGW(TAG, "downloaded File Size %d", content_length);
+
+        char *buffer = malloc(content_length);
+        if (buffer == NULL)
+        {
+            ESP_LOGE(TAG, "Failed to allocate memory for download buffer");
+        }
+        else
+        {
+            int total_read_len = 0, read_len;
+            while (total_read_len < content_length)
+            {
+                read_len = esp_http_client_read(client, buffer + total_read_len, content_length - total_read_len);
+                if (read_len <= 0)
+                {
+                    ESP_LOGE(TAG, "Error reading data");
+                    break;
+                }
+                total_read_len += read_len;
+            }
+
+            // Store the downloaded data in NVS
+            nvs_handle_t nvs_handle;
+            err = nvs_open("storage", NVS_READWRITE, &nvs_handle);
+            if (err != ESP_OK)
+            {
+                ESP_LOGE(TAG, "Error opening NVS");
+            }
+            else
+            {
+                err = nvs_set_str(nvs_handle, "downloaded_file", buffer);
+                if (err != ESP_OK)
+                {
+                    ESP_LOGE(TAG, "Error writing to NVS");
+                }
+                nvs_close(nvs_handle);
+            }
+
+            free(buffer);
+        }
+    }
+    esp_http_client_close(client);
+    esp_http_client_cleanup(client);
+}
+
+void app_main(void)
+{
+    // Initialize NVS (Non-Volatile Storage)
+    esp_err_t ret = nvs_flash_init();
+    // If no free pages or new version found, erase NVS partition and reinitialize
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // Log the WiFi mode
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    // Initialize the WiFi station
+    wifi_init_sta();
+
+    // Create a new task for the HTTP GET request
+    // xTaskCreate(&http_get_task, "http_get_task", 4096, NULL, 5, NULL);
+
+    // Create a new task for logging every 100ms
+    // xTaskCreate(&log_task, "log_task", 4096, NULL, 5, NULL);
+
+    // Call the new function with the URL of the 'New_SW.txt' file in the GitHub repo
+
+    nvs_stats_t nvs_stats;
+    nvs_get_stats(NULL, &nvs_stats);
+    ESP_LOGE(TAG,"Total entries: %d\n", nvs_stats.total_entries);
+    ESP_LOGE(TAG,"Used entries: %d\n", nvs_stats.used_entries);
+    ESP_LOGE(TAG,"Free entries: %d\n", nvs_stats.free_entries);
+    ESP_LOGE(TAG,"Namespace count: %d\n", nvs_stats.namespace_count);
+
+    download_and_log_file("https://raw.githubusercontent.com/hazem3443/OTA/master/Version.json");
+    download_and_log_file("https://raw.githubusercontent.com/hazem3443/OTA/master/OTA.bin");
+}
